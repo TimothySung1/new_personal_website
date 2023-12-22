bb2e8d103a9b6e65571e79e498d0fb9277d8c160
Timothy Sung <tsung8@gatech.edu>
2023-12-22 14:03:56 -0500

    added layout component, modified routing to automatically redirect to /home

--- new-personal-website/src/main.jsx
+++ new-personal-website/src/main.jsx
@@ -2,20 +2,40 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {
   createBrowserRouter,
-  RouterProvider
+  RouterProvider,
+  redirect
 } from 'react-router-dom'
 import App from './App.jsx'
 import Test from './pages/Test.jsx'
 import './index.css'
+import RootLayout from './components/RootLayout.jsx'
+
+const redirector = async () => {
+  return redirect('/home');
+}
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <App />
+    element: <RootLayout />,
+    children: [
+      {
+        index: true,
+        loader: redirector,
+      },
+      {
+        path: 'home',
+        element: <App />,
+      }
+    ],
   },
   {
     path: '/test',
     element: <Test />
+  },
+  {
+    path: '*',
+    loader: redirector,
   }
 ])
 
